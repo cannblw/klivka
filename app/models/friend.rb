4f504c0bb1e9e6871b11386d2ea34ad4f5481c90
Edgar Chirivella <edgar.95.12@gmail.com>
2026-08-05 08:29:31 +0100

    Add friendly_id to Friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -19,11 +19,18 @@
 #  user_id  (user_id => users.id)
 #
 class Friend < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: [ :slugged, :scoped ], scope: :user
+
   belongs_to :user
   has_many :entries, dependent: :destroy
   has_many :interactions, dependent: :destroy
 
   validates :name, presence: true
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 
   def initials
     name.split.take(2).map { |part| part[0] }.join.upcase
