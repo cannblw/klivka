47a96c377f0133fc61102c34930273cc3df261d5
Edgar Chirivella <edgar.95.12@gmail.com>
2026-08-19 21:09:00 +0100

    Make reminder mail previews self-contained

--- test/mailers/previews/reminder_mailer_preview.rb
+++ test/mailers/previews/reminder_mailer_preview.rb
@@ -1,26 +1,52 @@
 class ReminderMailerPreview < ActionMailer::Preview
   def keep_in_touch
-    ReminderMailer.with(delivery: delivery_for("Brittany Klocko", source: :keep_in_touch)).keep_in_touch
+    ReminderMailer.with(delivery: delivery_for(keep_in_touch_source)).keep_in_touch
   end
 
   def birthday
-    ReminderMailer.with(delivery: delivery_for("Mathew Schumm", source: "Entry::Birthday")).birthday
+    ReminderMailer.with(delivery: delivery_for(birthday_source)).birthday
   end
 
   def significant_date
-    ReminderMailer.with(delivery: delivery_for("Alec Leuschke", source: "Entry::Date")).significant_date
+    ReminderMailer.with(delivery: delivery_for(significant_date_source)).significant_date
   end
 
   private
 
-  def delivery_for(friend_name, source:)
-    friend = User.find_by!(email_address: Rails.application.config.x.development_seed_email_address)
-      .friends.find_by!(name: friend_name)
-    source = if source == :keep_in_touch
-      friend.keep_in_touch_setting
-    else
-      friend.entries.find_by!(type: source).entry_reminder
-    end
-    ReminderDelivery.find_by!(source_type: source.class.polymorphic_name, source_id: source.id, channel: "email")
+  def delivery_for(source)
+    occurrence_on = Date.current + 14.days
+    ReminderDelivery.new(
+      user: preview_user,
+      source:,
+      channel: ReminderDelivery::EMAIL_CHANNEL,
+      reminder_on: occurrence_on,
+      occurrence_on:
+    )
+  end
+
+  def preview_user
+    @preview_user ||= User.find_by!(email_address: Rails.application.config.x.development_seed_email_address)
+  end
+
+  def preview_friend
+    @preview_friend ||= preview_user.friends.order(:id).first!
+  end
+
+  def keep_in_touch_source
+    KeepInTouchSetting.new(friend: preview_friend, cadence: KeepInTouchSetting::DEFAULT_CADENCE, enabled_on: Date.current)
+  end
+
+  def birthday_source
+    birthday = Entry::Birthday.joins(:friend).find_by!(friends: { user_id: preview_user.id })
+    EntryReminder.new(entry: birthday)
+  end
+
+  def significant_date_source
+    entry = Entry::Date.new(
+      friend: preview_friend,
+      entry_date: Date.current + 14.days,
+      content: { "label" => "A date worth remembering" }
+    )
+    EntryReminder.new(entry: entry)
   end
 end
---
+++ test/mailers/reminder_mailer_preview_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class ReminderMailerPreviewTest < ActionMailer::TestCase
+  test "renders every reminder email from basic development seed data without creating reminder records" do
+    user = User.create!(
+      email_address: Rails.application.config.x.development_seed_email_address,
+      password: "preview-password",
+      time_zone: "UTC"
+    )
+    friend = user.friends.create!(name: "Preview Friend")
+    Entry::Birthday.create!(friend:, entry_date: Date.new(1990, 9, 14))
+    record_counts = reminder_record_counts
+
+    preview = ReminderMailerPreview.new
+    messages = [ preview.keep_in_touch, preview.birthday, preview.significant_date ]
+
+    assert messages.all?(&:multipart?)
+    assert_equal record_counts, reminder_record_counts
+  end
+
+  private
+
+  def reminder_record_counts
+    [ KeepInTouchSetting.count, EntryReminder.count, ReminderDelivery.count ]
+  end
+end
